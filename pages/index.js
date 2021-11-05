30ef6fae2e8ba561eb212eebb3a4dbd6a6ec9ce0
Tansi Jones <tansijones@outlook.com>
2021-11-05 20:08:43 +0200

    Fixed sticky navbar bug

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,34 @@
+import React, { useState, useEffect } from "react";
+
 import Banner from "../components/banner/Banner";
 import DocHead from "../components/head/DocHead";
 import Navbar from "../components/navbar/Navbar";
 import Partner from "../components/partner/Partner";
 
 export default function Home() {
+  const [navbackground, setNavbackground] = useState("bg-white");
+  const [onScroll, setOnScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setOnScroll(true);
+      } else {
+        setOnScroll(false);
+      }
+    });
+  }, []);
   return (
     <>
       {/* Document Head */}
       <DocHead />
 
       <div className="bg-zeroBlue">
-        <nav className="py-4">
+        <nav
+          className={`py-4 sticky top-0 z-50 ${
+            onScroll && navbackground
+          } transition transform ease-in-out duration-400`}
+        >
           <Navbar />
         </nav>
         <div className="w-[90%] mx-auto font-lato tracking-wider">
